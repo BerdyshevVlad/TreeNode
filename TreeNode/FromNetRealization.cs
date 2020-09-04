34951f457c65e607b1933a88350a78ade9132904
Vlad Berdyshev <daryaaay@gmail.com>
2020-09-04 13:18:36 +0300

    Add project files.

---
+++ TreeNode/FromNetRealization.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreeNode
+{
+    class FromNetRealization
+    {
+        public class MyObject
+        {   // The actual object
+            public int ParentId { get; set; }
+            public int Id { get; set; }
+        }
+
+        public class Node
+        {
+            public List<Node> Children = new List<Node>();
+            public Node Parent { get; set; }
+            public MyObject Source { get; set; }
+        }
+
+        public List<Node> BuildTreeAndGetRoots(List<MyObject> actualObjects)
+        {
+            var lookup = new Dictionary<int, Node>();
+            var rootNodes = new List<Node>();
+
+            foreach (var item in actualObjects)
+            {
+                // add us to lookup
+                Node ourNode;
+                if (lookup.TryGetValue(item.Id, out ourNode))
+                {   // was already found as a parent - register the actual object
+                    ourNode.Source = item;
+                }
+                else
+                {
+                    ourNode = new Node() { Source = item };
+                    lookup.Add(item.Id, ourNode);
+                }
+
+                // hook into parent
+                if (item.ParentId == 0)
+                {   // is a root node
+                    rootNodes.Add(ourNode);
+                }
+                else
+                {   // is a child row - so we have a parent
+                    Node parentNode;
+                    if (!lookup.TryGetValue(item.ParentId, out parentNode))
+                    {   // unknown parent, construct preliminary parent
+                        parentNode = new Node();
+                        lookup.Add(item.ParentId, parentNode);
+                    }
+                    parentNode.Children.Add(ourNode);
+                    ourNode.Parent = parentNode;
+                }
+            }
+
+            return rootNodes;
+        }
+
+        public List<MyObject> SetMyObjects()
+        {
+            var items2 = new List<MyObject>()
+            {
+                new MyObject()
+                {
+                    Id=1,
+                    ParentId=0
+                },
+                new MyObject()
+                {
+                    Id=2,
+                    ParentId=1
+                },
+                new MyObject()
+                {
+                    Id=3,
+                    ParentId=2
+                },
+                new MyObject()
+                {
+                    Id=4,
+                    ParentId=1
+                },
+            };
+
+            return items2;
+        }
+    }
+}
---
+++ TreeNode/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TreeNode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var items = new List<Item>()
+            {
+                new Item()
+                {
+                    Id=1,
+                    ParentId=4
+                },
+                new Item()
+                {
+                    Id=2,
+                    ParentId=5
+                },
+                new Item()
+                {
+                    Id=3,
+                    ParentId=1
+                },
+                new Item()
+                {
+                    Id=4,
+                    ParentId=null
+                },
+                new Item()
+                {
+                    Id=5,
+                    ParentId=4
+                },
+
+                new Item()
+                {
+                    Id=6,
+                    ParentId=4
+                },
+                new Item()
+                {
+                    Id=7,
+                    ParentId=null
+                },
+                new Item()
+                {
+                    Id=8,
+                    ParentId=7
+                },
+                new Item()
+                {
+                    Id=9,
+                    ParentId=8
+                }
+            };
+
+
+
+            SortedDictionary<int, Leaf> leafCollection = new SortedDictionary<int, Leaf>();
+            List<Leaf> root = new List<Leaf>();
+            bool isRootExist = false;
+
+            foreach (var item in items)
+            {
+                Leaf parentNode;
+                Leaf currentLeaf = new Leaf
+                {
+                    Id = item.Id,
+                    ParentId = item.ParentId
+                };
+                if (item.ParentId.HasValue)
+                {
+                    if (!leafCollection.TryGetValue(item.Id, out Leaf existedLeaf))
+                    {
+                        leafCollection.Add(item.Id, currentLeaf);
+                    }
+                    else
+                    {
+                        currentLeaf = existedLeaf;
+                        currentLeaf.ParentId = item.ParentId;
+                    }
+
+                    if (!leafCollection.TryGetValue(item.ParentId.Value, out parentNode))
+                    {
+                        parentNode = new Leaf();
+                        parentNode.Id = item.ParentId.Value;
+                        leafCollection.Add(item.ParentId.Value, parentNode);
+                    }
+                    parentNode.Children.Add(currentLeaf);
+
+                }
+                else
+                {
+                    if (leafCollection.TryGetValue(item.Id, out parentNode))
+                    {
+
+                        root.Add(parentNode);
+                    }
+                    else
+                    {
+                        leafCollection.Add(item.Id, currentLeaf);
+                        root.Add(currentLeaf);
+                    }
+                    isRootExist = true;
+                }
+            }
+
+            if (!isRootExist)
+            {
+                var rootNode = leafCollection.Where(x => x.Value.ParentId == leafCollection.First().Key);
+                foreach (var item in rootNode)
+                {
+                    leafCollection.TryGetValue(item.Key, out Leaf parentNode);
+                    root.Add(parentNode);
+                }
+
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Item
+    {
+        public int Id { get; set; }
+        public int? ParentId { get; set; }
+    }
+
+    public class Leaf
+    {
+        public int Id { get; set; }
+        public int? ParentId { get; set; }
+        public List<Leaf> Children { get; set; }
+
+        public Leaf()
+        {
+            Children = new List<Leaf>();
+        }
+    }
+}
